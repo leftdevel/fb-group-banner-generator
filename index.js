40cf1732ce18f00c5333efc204d016d138669765
Oscar Balladares <leftdevel@gmail.com>
2020-05-09 22:23:03 -0600

    jpg

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import https from "https";
 import dotenv from "dotenv";
 import puppeteer from "puppeteer";
 import fs from "fs";
+import path from "path";
 // import Preview from "./src/Preview";
 
 dotenv.config();
-
 
 // const server = Preview();
 
@@ -21,23 +21,25 @@ const passSelector = "#pass";
 const submitSelector = "#login_form input[type='submit']";
 // https://www.facebook.com/groups/197973700229538/insights/?section=members
 const groupUrl = "https://www.facebook.com/groups/197973700229538/members/";
-const imgClasses = "_s0 _4ooo img"; // ["img", "_5_4e"];
-// const maxPhotos = 10;
+const imgClasses = "img._s0._4ooo.img"; // ["img", "_5_4e"];
+const maxPhotos = 10;
+const destinationDir = "public";
 
 const download = async (url, destination) => new Promise((resolve, reject) => {
   const file = fs.createWriteStream(destination);
 
-  https.get(url, (response) => {
-    response.pipe(file);
+  return https
+    .get(url, (response) => {
+      response.pipe(file);
 
-    file.on("finish", () => {
-      file.close(resolve(true));
+      file.on("finish", () => {
+        file.close(resolve(file));
+      });
+    })
+    .on("error", (error) => {
+      fs.unlink(destination);
+      reject(error.message);
     });
-  }).on("error", (error) => {
-    fs.unlink(destination);
-
-    reject(error.message);
-  });
 });
 
 async function main() {
@@ -52,11 +54,15 @@ async function main() {
   await page.click(submitSelector);
   await page.waitForNavigation();
   await page.goto(groupUrl);
-  const images = await page.evaluate(() => Array.from(document.querySelectorAll(imgClasses), (e) => e.src));
+  const images = await page.evaluate(
+    (selector) => Array.from(document.querySelectorAll(selector), (e) => e.src),
+    imgClasses,
+  );
 
-  for (let i = 0; i < images.length; i += 1) {
-    console.log(images[i]);
-    download(images[i], `image-${i}.png`);
+  const totalPhotos = Math.min(images.length, maxPhotos);
+
+  for (let i = 0; i < totalPhotos; i += 1) {
+    download(images[i], path.join([__dirname, destinationDir, `image-${i}.jpg`])).catch((err) => console.log(err));
   }
 
   await page.screenshot({ path: "facebook.png" });
