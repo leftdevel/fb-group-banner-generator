2258b4407a84dce2d06f0082d9bbad9fe3e62156
Oscar Balladares <leftdevel@gmail.com>
2020-05-17 20:41:47 -0600

    nunjuck templating

--- src/Painter.js
+++ src/Painter.js
@@ -1,40 +1,64 @@
-import http from "http";
+/* eslint-disable no-console */
 import path from "path";
 import fs from "fs";
+import express from "express";
+import nunjucks from "nunjucks";
 
-const publicDir = path.join(__dirname, "..", "public");
+const root = path.resolve(__dirname, "..");
+const templatesDir = `${root}/templates`;
+const imagesDir = `${root}/public/img`;
 
-const Painter = (port = 9615) => {
-  const index = fs.readFileSync("./assets/index.html");
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    // eslint-disable-next-line no-param-reassign
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 
-  const server = http
-    .createServer((req, res) => {
-      if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(index);
-      } else if (req.url.match(".css$")) {
-        const cssPath = path.join(publicDir, req.url);
-        const fileStream = fs.createReadStream(cssPath, "UTF-8");
-        res.writeHead(200, { "Content-Type": "text/css" });
-        fileStream.pipe(res);
-      } else if (req.url.match(".png$")) {
-        const imagePath = path.join(publicDir, req.url);
-        const fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, { "Content-Type": "image/png" });
-        fileStream.pipe(res);
-      } else if (req.url.match(".jpg$")) {
-        const imagePath = path.join(publicDir, req.url);
-        const fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, { "Content-Type": "image/jpeg" });
-        fileStream.pipe(res);
-      } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("No Page Found");
+  return array;
+};
+
+const Painter = () => {
+  const host = process.env.HOST || "127.0.0.1";
+  const port = process.env.PORT || 8080;
+  const app = express();
+
+  nunjucks.configure(templatesDir, {
+    autoescape: true,
+    express: app,
+  });
+
+  app.set("view engine", "njk");
+  app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+  });
+
+  app.get("/", (req, res) => {
+    fs.readdir(imagesDir, null, (err, files) => {
+      if (err) {
+        throw err;
       }
-    })
-    .listen(port);
 
-  return server;
+      if (files.length === 0) {
+        throw new Error("No images found");
+      }
+
+      const images = files.map((file) => `/img/${file}`);
+
+      res.render("banner", { images: shuffle(images) });
+    });
+  });
+
+  app.use(express.static(`${root}/public`));
+
+  const logging = () => {
+    console.log("Server started", host, port);
+    console.log("Root directory", root);
+    console.log("Press Ctrl+C to exit...\n");
+  };
+
+  return app.listen(port, host, logging);
 };
 
 export default Painter;
--- src/index.js
+++ src/index.js
@@ -10,19 +10,20 @@ dotenv.config();
 const banner = path.join(__dirname, "..", "banner.jpg");
 const uploadUrl = "https://www.facebook.com/groups/961212304309293";
 
-// async function main() {
-//   const port = 8080;
-//   const server = await Painter(port);
-//   const serverUrl = "http://localhost:8080";
+async function main() {
+  const server = await Painter();
+  // const serverUrl = "http://localhost:8080";
   // await Photographer(serverUrl, banner);
   // await server.close();
-// }
-
-async function upload() {
-  await Uploader(uploadUrl, banner);
 }
 
-upload();
+main();
+
+// async function upload() {
+//   await Uploader(uploadUrl, banner);
+// }
+
+// upload();
 
 // async function main() {
 //   const totalPhotos = await Scrapper();
